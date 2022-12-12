99b933088a4d5bb0ab46efc514ac46bde2f3fdd9
ozgenurcalis <97458865+ozgenurcalis@users.noreply.github.com>
2022-12-12 14:26:50 +0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,6 +1,10 @@
 void main() {
-  var name = 'Dart';
-  var number = 1;
-  var number2 = 11;
-  print(number2 + number);
+  double number_double = 2.0;
+  String name = "Dart";
+  String name2 = "\nI love to develope \napplications with Flutter";
+  String name3 = "The price is \$10";
+  print(name + name2);
+  print(name3);
+  bool isBiggerThanZero = number_double > 3;
+  print(isBiggerThanZero);
 }
