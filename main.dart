4999cfc16d416f1ffd599df4756768538603c8c7
ozgenurcalis <97458865+ozgenurcalis@users.noreply.github.com>
2023-01-07 21:44:46 +0300

    seventh commit

--- main.dart
+++ main.dart
@@ -1,23 +1,17 @@
 void main() {
-  for (var i = 1; i <= 10; i = i + 1) {
-    print(i);
-  }
-  var products = ["laptop", "mouse", "keyboard"];
-  for (var i = 0; i < products.length; i++) {
-    print(products[i]);
-  }
-  for (var product in products) {
-    print(product);
-  }
+  var dictionary1 = new Map();
+  dictionary1["book"] = "kitap";
+  dictionary1["little"] = "küçük";
+  var dictonary2 = {"kitap": "book", "küçük": "little"};
+  dictonary2["büyük"] = "big";
+  print(dictionary1);
+  print(dictonary2);
 
-  int sayi = 1;
-  while (sayi <= 10) {
-    print(sayi);
-    sayi++;
+  dictionary1.remove("book");
+  print(dictionary1);
+
+  for (var value in dictonary2.values) {
+    print(value);
   }
-  var sayi2 = 10;
-  do {
-    print("sayi2=" + sayi2.toString());
-    sayi2++;
-  } while (sayi2 > 1000);
+  print(dictonary2.containsKey("kitap"));
 }
